d7d5f77f20adf847b4517fe3c53000b7859c76a7
marc-gonzalez <marcgonzalez@outlook.com>
2017-11-29 11:17:01 -0800

    villain images folder added/villain array with objects created

---
+++ main.js
@@ -0,0 +1,20 @@
+var villains = [
+    {name: "Auric Goldfinger",
+     photo: "villain_images/a_Goldfinger.jpg",
+     trivia: ["Claims to be an expert pistol shot that never misses",
+              "Is a Jeweller and Smuggler,",
+              "Has a manservant named Oddjob"]
+    },
+    {name: "Alec Trevelyan",
+     photo: "villain_images/Alec_Trevelyan.jpg",
+     trivia: ["Formerly agent 006 of MI6",
+              "Also known as Janus",]
+    },
+    {name: "Raoul Silva",
+     photo: "villain_images/raoul_silva.png",
+     trivia: ["former partner of Olivia Mansfield (M)",
+              "Cyber-terrorist",
+              "Captured, tortured, and imprisoned by the Chinese"]
+    }
+
+];
