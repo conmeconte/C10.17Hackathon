6993743829f5a30557b373325f70fa3ef6ff0785
conmeconte <saja523@hotmail.com>
2017-11-29 11:26:36 -0800

    ready to merge

--- main.js
+++ main.js
@@ -5,48 +5,53 @@ var map;
 var fenway;
 var panorama;
 var locationObj=[
-    {name: "Istanbul", location: [41.0081056,28.9810191], youTubeId:null, trivia: null },
-    {name: "Venice", location: [45.4384184,12.3359239], youTubeId:null, trivia: null },
-    {name: "New York", location: [40.7536533,-73.9806382], youTubeId:null, trivia: null },
+    {id: 1, name: "London", location: [51.5005803,-0.1258119], youTubeId:null, trivia: null },
+    {id: 2, name: "Istanbul", location: [41.0081056,28.9810191], youTubeId:null, trivia: null },
+    {id: 3, name: "Venice", location: [45.4384184,12.3359239], youTubeId:null, trivia: null },
+    {id: 4, name: "New York", location: [40.7536533,-73.9806382], youTubeId:null, trivia: null },
+    {id: 5, name: "Tokyo", location: [35.7141231,139.7966704], youTubeId:null, trivia: null },
+    {id: 6, name: "Los Vegas", location: [36.1126258,-115.1767051], youTubeId:null, trivia: null },
+    {id: 7, name: "Cairo", location: [29.9778574,31.1287904], youTubeId:null, trivia: null },
+    {id: 8, name: "Beirut", location: [33.8969085,35.5023606], youTubeId:null, trivia: null },
+    {id: 9, name: "Moscow", location: [55.7530756,37.62217], youTubeId:null, trivia: null },
+    {id: 10, name: "Berlin", location: [52.5163767,13.3788291], youTubeId:null, trivia: null },
+    {id: 11, name: "Prague", location: [50.0875638,14.4212062], youTubeId:null, trivia: null },
+    {id: 12, name: "Havana", location: [23.1401843,-82.3508177], youTubeId:null, trivia: null },
+    {id: 13, name: "Shanghai", location: [31.239481,121.4998016], youTubeId:null, trivia: null },
+    {id: 14, name: "Rio De Janeiro", location: [-22.9522173,-43.2102183], youTubeId:null, trivia: null },
+    {id: 15, name: "Sydney", location: [-33.8590761,151.2221537], youTubeId:null, trivia: null }
+
 
 ];
 
 function createLocationButton(locations){
 
     for(var location_i=0; location_i<locations.length; location_i++){
-        var buttonElem=$('<button>');
-        locations[location_i].domElement=buttonElem;
+        var buttonElem=$('<button>').addClass("btn btn-primary");
+        var place=locations[location_i];
+        place.domElement=buttonElem;
         buttonElem.text(locations[location_i].name);
+        buttonElem.on('click',moveLocationsOnClick.bind(place));
         $('.locationBtns').append(buttonElem);
     }
 
 
 }
 
+function moveLocationsOnClick(){
+    map.setCenter({lat:  this.location[0], lng: this.location[1]});
+    map.streetView.setPosition({lat:  this.location[0], lng: this.location[1]});
+}
+
 function init(){
     createLocationButton(locationObj);
-    $('.Istanbul').on("click",function(){
-        map.setCenter({lat:  41.0081056, lng: 28.9810191});
-        map.streetView.setPosition({lat:  41.0081056, lng: 28.9810191});
-//                function markerLocation() {
-//                    var marker = new google.maps.Marker({
-//                        position: {lat: 28.9810191, lng: 28.9810191},
-//                        map: map
-//                    });
-//                    markers.push(marker);
-//                };
-//                markerLocation();
-    });
-    $('.Sydney').on("click",function() {
-        map.setCenter({lat: -33.8590761, lng: 151.2221537});
-        map.streetView.setPosition({lat: -33.8590761, lng: 151.2221537});
-    })
+
 };
 
 
 
 function mapCreate() {
-    fenway = {lat: 42.345573, lng: -71.098326};
+    fenway = {lat: 51.500316, lng:  -0.126370};
     map = new google.maps.Map(document.getElementById('map'), {
         center: fenway,
         zoom: 10
