f8bde5006018d8db50cbbe1511f4ceea85fa8ee2
unleashalicia <alicia.adams.evans@gmail.com>
2017-11-30 15:49:44 -0800

    Final modal works and calls winning and losing modals.  Will add video in later versions or off to side.

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ $(document).ready(init);
 //     responsiveVoice.speak("Hello double oh seven. Thank you for coming. We have intercepted a message with information that could start the next world war. Please find out who is behind this and stop them!");
 // }
 
+var win = 0;
 //map variables
 var map;
 var fenway;
@@ -166,13 +167,17 @@ function handleClicks(){
         loadMovieFromServer(selectedVillain.movie);
         locationCounter=0;
         wrongChoiceCounter=0;
+        win=0;
     });
     $('.villainNames').click(function(){
         chooseMastermind(moviePoster);
     });
     $('.temporaryFinalModal').click(function(){
         $('#finalModal').css('display','block');
-    })
+    });
+    $('.closeModal').click(function(){
+        $('#finalModal').css('display','none');
+    });
 }
 
 function randomizer(arr){ //pass in villains array to generate a random villains object from villains array.
@@ -229,6 +234,7 @@ function chooseMastermind(poster){ //pass in movie poster
     var foundVillainIndex = $(event.target).index();
     var choice = $(event.target).text();
     if(choice === selectedVillain.name){
+        win = 2;
         $('.villainPics.v0').css({
             'background-image': 'url('+villains[foundVillainIndex].photo+')',
             'background-size': 'cover',
@@ -243,8 +249,10 @@ function chooseMastermind(poster){ //pass in movie poster
         $('.villainNames.v0').text(villains[foundVillainIndex].name);
         $('.villainNames.v1').hide();
         $('.villainNames.v2').text(villains[foundVillainIndex].movie);
+        setTimeout(5000, winningModal);
     } else {
-        //trigger loser modal.
+        win = 1;
+        losingModal();
     }
 
 }
@@ -300,17 +308,24 @@ function nextLocation(){
 /*If player clicks wrong choice more than 5 times this function trigger*/
 
 function losingModal(){
-    if(locationCounter+wrongChoiceCounter>=5){
+    if(locationCounter+wrongChoiceCounter>=5 && win===0){
         $('#initialModal p').text("You Lose");
         $('#initialModalImg').attr("src", "img/lose.gif").height("15vh").width("20vw");
         $('#initialModal').css('display', 'block');
         $('#missionButton').text('Try Again');
 
+    } else if (win===1){
+        $('#finalModal').css('display','none');
+        $('#initialModal p').text("You Lose");
+        $('#initialModalImg').attr("src", "img/lose.gif").height("15vh").width("20vw");
+        $('#initialModal').css('display', 'block');
+        $('#missionButton').text('Try Again');
     }
 }
 
 
 function winningModal(){
+        $('#finalModal').css('display', 'none');
         $('#initialModal p').text("You Got the Villain");
         $('#initialModalImg').attr("src", "img/Bond-appeal_wide.gif").height("15vh").width("20vw");
         $('#initialModal').css('display', 'block');
